b96b4c73fe9c351ae0f5d78440177a4ff43afd76
rebefumi <rebefumi@gmail.com>
2015-06-17 09:36:44 +0200

    Created a GameOver screen

---
+++ JavaScript/goHome/js/states/GameOverState.js
@@ -0,0 +1,22 @@
+var ChickenGame = ChickenGame || {};
+
+ChickenGame.GameOverState = {
+    init: function (someVariable) {
+        this.level = someVariable;
+    },
+    create: function (){
+            var style = {font: '30px Arial', fill:'#fff'};
+            this.add.text(this.game.width/2, this.game.height/2, 'GAME OVER', style).anchor.setTo(0.5);
+            var style = {font: '20px Arial', fill:'#fff'};
+            this.add.text(this.game.width/2, this.game.height/2 + 50, 'High socre: ' + this.level, style).anchor.setTo(0.5);
+            var style = {font: '20px Arial', fill:'#fff'};
+            this.add.text(this.game.width/2, this.game.height/2 + 120, 'Tap to play again', style).anchor.setTo(0.5);
+
+            this.game.input.onDown.addOnce(this.restart, this);
+
+    },
+    restart: function (){
+        this.game.state.start("GameState");
+    }
+
+};
