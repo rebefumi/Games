9a5e2a834100af56f181cf61e2eaf4219f463491
rebefumi <rebefumi@gmail.com>
2015-06-17 09:37:15 +0200

    Added a sprite for the cars.

--- JavaScript/goHome/js/states/PreloadState.js
+++ JavaScript/goHome/js/states/PreloadState.js
@@ -9,6 +9,9 @@ ChickenGame.PreloadState = {
         this.preloadBar.anchor.setTo(0.5);
         this.load.setPreloadSprite(this.preloadBar);
 
+        this.load.text('level', 'assets/data/level.json');
+        this.load.text('constant', 'assets/data/constant.json');
+
         //load all images
         this.load.image('home_background', 'assets/images/farm_landscape.png');
         this.load.image('home_title', 'assets/images/go_home.png');
@@ -16,13 +19,11 @@ ChickenGame.PreloadState = {
         this.load.image('live_empty', 'assets/images/live_empty.png');
         this.load.image('live', 'assets/images/live_full.png');
         this.load.image('background', 'assets/images/road.png');
-        this.load.image('carBlue', 'assets/images/cars/car_blue.png');
+
+        this.load.spritesheet('car', 'assets/images/spreadsheet.png', 81, 40, 11, 0 , 0);
 
         this.load.spritesheet('chicken', 'assets/images/chicken_sprite.png', 27, 40, 4);
 
-
-        this.load.text('level', 'assets/data/level.json');
-        this.load.text('constant', 'assets/data/constant.json');
     },
     create: function (){
         this.state.start('HomeState');
