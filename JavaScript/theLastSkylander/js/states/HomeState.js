7542fc7d8f03b426b2f5dbd3c07a21f7fba7aeb4
rebefumi <rebefumi@gmail.com>
2015-06-30 20:45:38 +0200

    Fit the background

--- JavaScript/theLastSkylander/js/states/HomeState.js
+++ JavaScript/theLastSkylander/js/states/HomeState.js
@@ -4,6 +4,8 @@ skylander.HomeState = {
     create: function (){
         var background = this.game.add.sprite(0,0, 'backHome');
         background.inputEnabled = true;
+        background.width = this.game.width;
+        background.height = this.game.height;
         background.events.onInputDown.add(function (){
             this.state.start('GameState');
         }, this);
