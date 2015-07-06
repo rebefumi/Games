aa8316f8e687b790bda965c0e11621f672880541
rebefumi <rebefumi@gmail.com>
2015-07-06 13:38:06 +0200

    Prepared the game for multiple levels. Loaded all images from the game. Some minor changes in the Home State.

--- JavaScript/MilvasScape/js/states/HomeState.js
+++ JavaScript/MilvasScape/js/states/HomeState.js
@@ -3,11 +3,11 @@ var RPG = RPG || {}
 RPG.HomeState = {
     create: function (){
         var background = this.game.add.sprite(0,0, 'backHome');
-        background.width = this.game.width;
-        background.height = this.game.height;
+        background.width = this.game.world.width;
+        background.height = this.game.world.height;
         background.inputEnabled = true;
 
-        var title = this.game.add.sprite(50, 40, 'title');
+        var title = this.game.add.sprite(0, 0, 'title');
 
         var style = {font:'18px Arial', fill: '#fff'};
         this.game.add.text(this.game.world.centerX + 50, this.game.world.height - 50, 'TOUCH TO START', style);
--- JavaScript/MilvasScape/js/states/PreloadState.js
+++ JavaScript/MilvasScape/js/states/PreloadState.js
@@ -9,16 +9,47 @@ RPG.PreloadState = {
         this.preloadBar.anchor.setTo(0.5);
         this.load.setPreloadSprite(this.preloadBar);
 
-        //load all images
-        this.load.image('backHome', 'assets/images/backHome.jpg');
-        this.load.image('title', 'assets/images/title.png');
+        if (!this.loadLevel) {
+            //load all images
+            this.load.image('backHome', 'assets/images/backHome.jpg');
+            this.load.image('title', 'assets/images/title.png');
 
-        this.load.spritesheet('player', 'assets/images/player.png', 64, 64, 273);
+            this.load.spritesheet('player', 'assets/images/player.png', 64, 64, 273);
 
-        //load json
-        this.load.text('level', 'assets/data/level.json');
+            this.load.text('constants', 'assets/data/constants.json');
+
+            //load game assets
+            this.load.image('sword', 'assets/images/attack-icon.png');
+            this.load.image('quest', 'assets/images/quest-button.png');
+            this.load.image('chest', 'assets/images/chest-gold.png');
+            this.load.image('coin', 'assets/images/coin.png');
+            this.load.image('potion', 'assets/images/potion.png');
+            this.load.image('shield', 'assets/images/shield.png');
+            this.load.image('scroll', 'assets/images/scroll-skull.png');
+            this.load.image('strangeItem', 'assets/images/gods-helmet.png');
+
+            this.load.image('monster', 'assets/images/demon.png');
+            this.load.image('dragon', 'assets/images/goldendragon.png');
+            this.load.image('snake', 'assets/images/snake.png');
+            this.load.image('skeleton', 'assets/images/swordskeleton.png');
+            this.load.image('sword', 'assets/images/attack-icon.png');
+
+            this.load.image('tilesheet', 'assets/images/terrains.png');
+
+
+        }
+        //load map
+        this.load.tilemap(this.currentLevel, 'assets/data/' + this.currentLevel + '.json', null, Phaser.Tilemap.TILED_JSON);
+    },
+    init: function (level){
+        this.currentLevel = level || 'world';
+        this.loadLevel = level ? true : false;
     },
     create: function (){
-        this.state.start('HomeState');
+        if (this.loadLevel){
+            this.state.start('GameState', true, false, this.currentLevel)
+        }else {
+            this.state.start('HomeState');
+        }
     }
 };
